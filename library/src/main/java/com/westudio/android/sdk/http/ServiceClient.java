8dcdebcec6f998f5f8f053c90708bc0ef9b4ccd3
TonyAdo <coolhzb@163.com>
2014-08-05 15:22:45 +0800

    update the ServiceClient part....

--- library/src/main/java/com/westudio/android/sdk/http/ServiceClient.java
+++ library/src/main/java/com/westudio/android/sdk/http/ServiceClient.java
@@ -1,5 +1,25 @@
 package com.westudio.android.sdk.http;
 
+import com.westudio.android.sdk.loopj.android.http.AsyncHttpClient;
+
 public class ServiceClient {
-    
+
+    private static volatile ServiceClient client = null;
+    private AsyncHttpClient httpClient = null;
+
+    private ServiceClient() {
+        httpClient = new AsyncHttpClient();
+    }
+
+    public static ServiceClient getInstance() {
+        if (client == null) {
+            synchronized (ServiceClient.class) {
+                if (client == null) {
+                    client = new ServiceClient();
+                }
+            }
+        }
+
+        return client;
+    }
 }
